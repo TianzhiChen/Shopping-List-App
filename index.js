278345fb66f81f10eee38a948350308dc9094cf5
Tianzhi Chen <chen.tianzh@northeastern.edu>
2023-07-07 00:28:52 -0400

    getting id if item in database

--- index.js
+++ index.js
@@ -33,15 +33,25 @@ function(snapshot) {} as the second argument
 
 onValue(shoppingListInDB, function(snapshot) {
     // Use Object.values() to convert snapshot.val() from an Object to an Array. Create a variable for this.
-    let itemsArray = Object.values(snapshot.val())
+    let itemsArray = Object.entries(snapshot.val())
+
+    // console.log(snapshot.val())
 
     clearShoppingListEl()  
     // console.log(itemsArray) 
 
     // Write a for loop to iterate on itemsArray and console log each item
     for (let i = 0; i < itemsArray.length; i++) {
+        let currentItem = itemsArray[i]
+
+        // Make two let variables:
+        // currentItemID and currentItemValue and use currentItem to set both of
+        // them equal to the correct values.
+        let currentItemID = currentItem[0]
+        let currentItemValue = currentItem[1]
+
         // Use the appendItemToShoppingListEl(itemValue) function inside of the for loop to append item to the shopping list element for each iteration.
-        appendItemToShoppingListEl(itemsArray[i])
+        appendItemToShoppingListEl(currentItemValue)
     }        
 })
 
